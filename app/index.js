bebb51e6e834654c05ab8c95de8e48acc78d23eb
theoldone <rouvaf123@gmail.com>
2024-08-25 14:17:53 +0100

    feat: button functionality
    Left and right button change menu name

--- app/index.js
+++ app/index.js
@@ -1,17 +1,19 @@
 var meal_name;
+var left_button = document.getElementById("left_button");
+var right_button = document.getElementById("right_button");
 meal_name = document.getElementsByClassName("meal_name")[0];
-
-// alert(`${meal_name}`);
+var menu_items_list_index = 0;
+var menu_items_list = ["Meals", "Chicken", "Sides", "Dessert & Drinks"];
 
 meal_name.addEventListener("click", () => {
-  alert("Here");
+  // alert("Here");
   show_menu();
 });
 
 function show_menu() {
-  var item_container = document.getElementByClassName("item_container")[0];
-  item_container.innerHTML = `
-        <div class="item">
+  var item_container = document.getElementsByClassName("item_container")[0];
+  var item = `
+          <div class="item">
               <div class="item_img">
                 <img
                   src="images/Chicken_Republic-New-Spicy-ChickWhizz-Meal.jpg"
@@ -25,6 +27,22 @@ function show_menu() {
               <div class="item_button">
                 <button>Order</button>
               </div>
-            </div>
+          </div>
     `;
+
+  item_container.innerHTML += item.trim();
 }
+
+left_button.addEventListener("click", () => {
+  if (!(menu_items_list_index < 1)) {
+    menu_items_list_index -= 1;
+    meal_name.innerHTML = `<p>${menu_items_list[menu_items_list_index]}</p>`;
+  }
+});
+
+right_button.addEventListener("click", () => {
+  if (!(menu_items_list_index > menu_items_list.length - 2)) {
+    menu_items_list_index += 1;
+    meal_name.innerHTML = `<p>${menu_items_list[menu_items_list_index]}</p>`;
+  }
+});
